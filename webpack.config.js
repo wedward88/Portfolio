172c273c01458cb4f2386bc7363b998ba943a4df
wdunn <williamdunn195@gmail.com>
2019-06-25 21:29:39 -0400

    Initial commit, build most of page, add some responsiveness

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry: './js/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'js')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader'
+            }
+        ]
+    }
+};
