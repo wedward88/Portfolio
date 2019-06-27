46cd88161b05f0d67a642d8ffc8d130e3e227204
wdunn <williamdunn195@gmail.com>
2019-06-27 15:03:55 -0400

    Transition for arrow

--- js/main.js
+++ js/main.js
@@ -1,2 +1,31 @@
 import '../scss/reset.scss'
 import '../scss/style.scss'
+
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    // let bodyHeight = $("body").height() - $(window).height();
+    let prevScrollTop = 0;
+    window.onscroll = () => {
+        // console.log($(window).scrollTop());
+        let currScrollTop = $(window).scrollTop();
+
+        if (currScrollTop > prevScrollTop) {
+            if ($(window).scrollTop() > 850) {
+                $(".down-icon .iconify").css({
+                    "transform": "rotate(180deg)",
+                    "transition": "transform 1s",
+                })
+            } 
+        } else {
+            
+            if ($(window).scrollTop() < 850) {
+                $(".down-icon .iconify").css({
+                    transform: "rotate(0deg)",
+                    transition: "transform 1s",
+                })
+            } 
+        }
+        prevScrollTop = currScrollTop;
+    };
+});
+
