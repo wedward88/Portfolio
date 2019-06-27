5c33ed0e74fd3a9f3b98a6abf3979ad33d375a01
wdunn <williamdunn195@gmail.com>
2019-06-27 15:22:04 -0400

    Adjust scroll flip mechanics

--- js/main.js
+++ js/main.js
@@ -5,12 +5,15 @@ import '../scss/style.scss'
 document.addEventListener('DOMContentLoaded', ()=> {
     // let bodyHeight = $("body").height() - $(window).height();
     let prevScrollTop = 0;
+
+    
     window.onscroll = () => {
         // console.log($(window).scrollTop());
         let currScrollTop = $(window).scrollTop();
-
+        console.log(currScrollTop);
+        let winHeight = $(window).height() / 2;
         if (currScrollTop > prevScrollTop) {
-            if ($(window).scrollTop() > 850) {
+            if ($(window).scrollTop() > winHeight) {
                 $(".down-icon .iconify").css({
                     "transform": "rotate(180deg)",
                     "transition": "transform 1s",
@@ -18,7 +21,7 @@ document.addEventListener('DOMContentLoaded', ()=> {
             } 
         } else {
             
-            if ($(window).scrollTop() < 850) {
+            if ($(window).scrollTop() > winHeight) {
                 $(".down-icon .iconify").css({
                     transform: "rotate(0deg)",
                     transition: "transform 1s",
