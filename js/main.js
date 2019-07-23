d5b80dab7b859569ec077db5fc66c5ef883399e3
wdunn <williamdunn195@gmail.com>
2019-07-22 21:48:45 -0400

    Refactor and fix arrow spin, adjust responsiveness

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ import '../scss/style.scss'
 document.addEventListener('DOMContentLoaded', ()=> {
 
     ///////// Arrow flip animation ////////////
-    let prevScrollTop = 0;
     
     window.onscroll = () => {
         let currScrollTop = $(window).scrollTop();
@@ -13,23 +12,9 @@ document.addEventListener('DOMContentLoaded', ()=> {
         let currScrollMid = (currScrollTop + winHeight / 2)
         let arrow = $(".down-icon .iconify");
         
-        let arrowDown = currScrollMid > arrow.offset().top
-        
-        // console.log(arrowDown);
+        let arrowUp = currScrollMid > arrow.offset().top
+        arrow.toggleClass("flipped", arrowUp)
 
-        if (arrowDown) {
-            arrow.css({
-                "transform": "rotate(180deg)",
-                "transition": "transform 1s",
-            })
-        } else {
-            arrow.css({
-                transform: "rotate(0deg)",
-                transition: "transform 1s",
-            })
-        }
-        
-        prevScrollTop = currScrollTop;
     };
 
     /////// END Arrow flip animation END ////////
@@ -55,28 +40,5 @@ document.addEventListener('DOMContentLoaded', ()=> {
     });
 
     ////////// END Project Hover Event Listeners END //////////////
-
-    ////////// Mobile Event Listeners /////////////////////
-
-    endeavCon[0].addEventListener('touchstart', () => {
-        endeavImg.attr("src", "https://media.giphy.com/media/kBqiBso7bdMDIicTbc/giphy.gif");
-    });
-
-    endeavCon[0].addEventListener('touchend', () => {
-        endeavImg.attr("src", "https://i.imgur.com/rYCcQVl.png");
-    });
-
-    skateCon[0].addEventListener('touchstart', () => {
-        skateImg.attr("src", "https://media.giphy.com/media/ibjeJHmzXjPFdJlpbW/giphy.gif")
-    });
-
-    skateCon[0].addEventListener('touchend', () => {
-        skateImg.attr("src", "https://i.imgur.com/MLum5CJ.png")
-    });
-
-
-
-
-
 });
 
